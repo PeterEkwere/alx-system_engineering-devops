d9b732d5f406bb840d6797f97fc94c63e7dfe087
PeterEkwere <peterekwere04@gmail.com>
2023-08-01 13:36:07 -0700

    initial commit -m for task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[\w*:(\w*)\] \[\w*:(\+\d*)\] \[\w*:(-1:0:-1:0:-1)\]/).join(',')
+puts ARGV[0].scan(/\[from:(\w*)\] \[to:(\+\d*)\] \[flags:(.*?)\]/).join(',')
