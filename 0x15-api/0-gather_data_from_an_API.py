12576ed8444432daa0e4c3202ab474938179b76c
PeterEkwere <peterekwere04@gmail.com>
2023-10-25 08:06:05 -0700

    updated task 1 module documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,40 +1,41 @@
 #!/usr/bin/python3
 """
-    This module gets an info on an employee using REAST API
+    This module gets an info on an employee using REST API
     Author: Peter Ekwere
 """
 import requests
 import sys
 
-user_id = sys.argv[1]
 
-user_todo = requests.get(f'https://jsonplaceholder.typicode.com/users/'
-                         f'{user_id}/todos')
-user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/'
-                         f'{user_id}')
+if __name__ == "__main__":
+    """ if module is run directly DO """
+    user_id = sys.argv[1]
+    user_todo = requests.get(f'https://jsonplaceholder.typicode.com/users/'
+                             f'{user_id}/todos')
+    user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/'
+                             f'{user_id}')
 
-if user_todo.status_code == 200:
-    if user_info.status_code == 200:
-        user_todo = user_todo.json()
-        user_info = user_info.json()
+    if user_todo.status_code == 200:
+        if user_info.status_code == 200:
+            user_todo = user_todo.json()
+            user_info = user_info.json()
 
-        user_name = user_info.get("name")
-        total_tasks = len(user_todo)
-        done_tasks = 0
-        titles = []
+            user_name = user_info.get("name")
+            total_tasks = len(user_todo)
+            done_tasks = 0
+            titles = []
 
-        for i in range(len(user_todo)):
-            if user_todo[i].get("completed") is True:
-                done_tasks += 1
-                titles.append(user_todo[i].get("title"))
-
-        print(f"Employee {user_name} is done with"
-              f"tasks({done_tasks}/{total_tasks}):")
-        for title in titles:
-            print(f"\t {title}")
+            for i in range(len(user_todo)):
+                if user_todo[i].get("completed") is True:
+                    done_tasks += 1
+                    titles.append(user_todo[i].get("title"))
+            print(f"Employee {user_name} is done with"
+                  f"tasks({done_tasks}/{total_tasks}):")
+            for title in titles:
+                print(f"\t {title}")
+        else:
+            print(f"Error: Unable to fetch data user info"
+                  f"(Status code {user_info.status_code})")
     else:
-        print(f"Error: Unable to fetch data user info"
-              f"(Status code {user_info.status_code})")
-else:
-    print(f"Error: Unable to fetch data user todo"
-          f"(Status code {user_todo.status_code})")
+        print(f"Error: Unable to fetch data user todo"
+              f"(Status code {user_todo.status_code})")
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    This module gets an info on an employee using REAST API
+    Author: Peter Ekwere
+"""
+import csv
+import requests
+import sys
+
+user_id = sys.argv[1]
+
+user_todo = requests.get(f'https://jsonplaceholder.typicode.com/users/'
+                         f'{user_id}/todos')
+user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/'
+                         f'{user_id}')
+
+if user_todo.status_code == 200:
+    if user_info.status_code == 200:
+        user_todo = user_todo.json()
+        user_info = user_info.json()
+
+        user_name = user_info.get("name")
+        data =[]
+
+        for i in range(len(user_todo)):
+            new_data = []
+            status = user_todo[i].get("completed")
+            title = user_todo[i].get("title")
+            new_data = [f'"{user_id}"', f'"{user_name}"', f'"{status}"', f'"{title}"']
+            data.append(new_data)
+
+        file_name = f"{user_id}.csv"
+        with open(file_name, "w") as csv_file:
+            csv_writer = csv.writer(csv_file)
+
+            csv_writer.writerows(data)
+    else:
+        print(f"Error: Unable to fetch data user info"
+              f"(Status code {user_info.status_code})")
+else:
+    print(f"Error: Unable to fetch data user todo"
+          f"(Status code {user_todo.status_code})")
