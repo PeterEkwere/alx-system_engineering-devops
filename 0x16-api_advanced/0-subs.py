beb7c5c496f29681dc691d086b0923de6b073590
Peter Ekwere <peterekwere04@gmail.com>
2023-11-08 12:39:35 -0800

    updated task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,4 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-
+    """ Returns the number of subs for a subreddit """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            "User-Agent": "v1.0.0"
+            }
+    response = request.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    res = response.json().get("data")
+    return res.get("subscribers")
